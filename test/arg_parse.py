e770ee6db7c4568657aca16fda38c39ff9a33600
webbhm <webb.howard@gmail.com>
2021-02-08 16:54:02 -0500

    Delete arg_parse.py

--- test/arg_parse.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='Control the Grow Light.')
-parser.add_argument("--a", help="Turn the light on or off", type=str, choices=["On", "Off", "on", "off"])
-args = parser.parse_args()
-if args.a in ["On", "on"]:
-    print("Light turned On")
-if args.a in ["Off", "off"]:
-    print("Light turned Off")
